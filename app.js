7e5789f425b4393655491009d147045148d5b123
root <root@localhost.localdomain>
2019-02-02 22:09:39 +0100

    created 2 endpoints

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var ep1Router = require('./routes/httpPage');
+var ep2Router = require('./routes/jsonPage');
 
 var app = express();
 
@@ -19,8 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/endpoint1', ep1Router);
+app.use('/endpoint2', ep2Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
