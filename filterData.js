495ca37b81e44fb1294df4a9aada7854c652f012
root <root@localhost.localdomain>
2019-02-03 20:18:09 +0100

    handled error parsing data

--- filterData.js
+++ filterData.js
@@ -12,33 +12,38 @@ function filterData(data, trendingOnly, callback) {
   
     //loop throw all posts
     for (let i = 0; i < objData.length; i++) {
-      let currentPost = objData[i];
+      try {
+        let currentPost = objData[i];
       
-      //check if it is trending
-      let trending = false;
-      if(!trendingOnly)
-        trending = true;
-      else 
-        for (let j= 0; j < currentPost['_categories'].length; j++)
-          if (currentPost['_categories'][j]['slug'] == 'trending') {
-            trending = true;
-            break;
-          }
+        //check if it is trending
+        let shouldPushThisPost = false;
+        if(!trendingOnly)
+          shouldPushThisPost = true;
+        else 
+          for (let j= 0; j < currentPost['_categories'].length; j++)
+            if (currentPost['_categories'][j]['slug'] == 'trending') {
+              shouldPushThisPost = true;
+              break;
+            }
   
-      //push to response
-      if (trending) {
-        imagePath = currentPost['_featuredImage']['baseUrl']
+        //push to response
+        if (shouldPushThisPost) {
+          imagePath = currentPost['_featuredImage']['baseUrl']
                   + currentPost['_featuredImage']['relativePath']
                   + currentPost['_featuredImage']['small'];
-        filtered.push({
-          _id: currentPost['_id'],
-          title: currentPost['seo']['title'],
-          image: imagePath,
-          shortDescription: currentPost['shortDescription'],
-          createdAt: currentPost['createdAt']
-        }); 
+          filtered.push({
+            _id: currentPost['_id'],
+            title: currentPost['seo']['title'],
+            image: imagePath,
+            shortDescription: currentPost['shortDescription'],
+            createdAt: currentPost['createdAt']
+          }); 
+        }
+      }  
+      catch (err) {
+        console.log('Post ${i} could not be parsed: '+err);
       }
-    }  
+    }
     callback(JSON.stringify(filtered));
 }
 
