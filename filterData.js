ac87c1a30cc9d409a11357005d7c0d41edc84fa2
root <root@localhost.localdomain>
2019-02-07 14:32:26 +0100

    fixed

--- filterData.js
+++ filterData.js
@@ -1,49 +1,34 @@
 //Gets important data from previous http request
-//if trendingOnly is true, do extra filtering
+//if trendingOnly is true, do extra filtering by trending
 function filterData(data, trendingOnly, callback) {
-    let filtered = [];
-  
     //parse and fetch data
     if(!data.hasOwnProperty('data')) {
       return callback({status:401, message: 'Problem with parsing data'});
     }
     let objData = data['data'];
-  
-    //loop throw all posts
-    for (let i = 0; i < objData.length; i++) {
-      try {
-        let currentPost = objData[i];
-      
-        //check if it is trending
-        let shouldPushThisPost = false;
-        if(!trendingOnly)
-          shouldPushThisPost = true;
-        else 
-          for (let j= 0; j < currentPost['_categories'].length; j++)
-            if (currentPost['_categories'][j]['slug'] == 'trending') {
-              shouldPushThisPost = true;
-              break;
-            }
-  
-        //push to response
-        if (shouldPushThisPost) {
-          imagePath = currentPost['_featuredImage']['baseUrl']
-                  + currentPost['_featuredImage']['relativePath']
-                  + currentPost['_featuredImage']['small'];
-          filtered.push({
-            _id: currentPost['_id'],
-            title: currentPost['seo']['title'],
-            image: imagePath,
-            shortDescription: currentPost['shortDescription'],
-            createdAt: currentPost['createdAt']
-          }); 
-        }
-      }  
-      catch (err) {
-        console.log('Post ${i} could not be parsed: '+err);
-      }
+
+    if(trendingOnly) { 
+      objData = objData.filter(filterByTrending);
     }
-    callback({status:200, message:'success', data:filtered});
+    
+    let reformatted = objData.map(post => {
+      imagePath = post['_featuredImage']['baseUrl']
+                  + post['_featuredImage']['relativePath']
+                  + post['_featuredImage']['small'];
+      let formatedPost = {
+        _id: post['_id'],
+        title: post['seo']['title'],
+        image: imagePath,
+        shortDescription: post['shortDescription'],
+        createdAt: post['createdAt']
+      };
+      return formatedPost;
+    });   
+    callback({status:200, message:'success', data:reformatted});
+}
+
+function filterByTrending(post) {
+  return post['_categories'].filter(post2 => post2['slug'] == 'trending').length > 0;
 }
 
 module.exports.execute = filterData;
